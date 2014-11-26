d370ce8793a73ae83f5419c18dfb46c361f26510
Balzer82 <balzer82@users.noreply.github.com>
2014-11-26 12:50:38 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'illuminaro',
+  packages = ['illuminaro'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Build interactive web applications with Python',
+  author = 'Artem Frolov',
+  author_email = '',
+  url = 'https://github.com/afrolov/illuminaro', # use the URL to the github repo
+  download_url = 'https://github.com/afrolov/illuminaro/archive/master.zip', # I'll explain this in a second
+  keywords = ['webapp', 'python', 'interactive'], # arbitrary keywords
+  classifiers = [],
+)
